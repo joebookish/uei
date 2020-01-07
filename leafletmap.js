8822f5096057da18f75af4cbad67e38db13b75e5
annajmorton <annajmorton@gmail.com>
2020-01-06 22:22:00 -0600

    doit

--- leafletmap.js
+++ leafletmap.js
@@ -1,215 +1,230 @@
 //global variables for testing
-var geojson_temp = [];
-var data_temp = [];
-var dataOptions_temp =[];
+var weight_matrix, morans_i_sd;
+var dataOptions, schools, mdata;
 
-// main loading function
-$.getJSON("acs5_variables.json", function(dataOptions){
-    $.getJSON("schools.json",function(schools){
-        $.getJSON("geo_2000.geojson", function(data) {
+// load map data
+$.when(
+    $.getJSON("weight_matrix_2000.json", function(data){
+        weight_matrix = data;
+    }),
 
-                // add moran's eye property
-                addGeojsonProp(data,"moran");
+    $.getJSON("morans_i_sd.json", function(data){
+        morans_i_sd = data;    
+    }),
 
-                //Init Overlays
-                var overlays = {};
+    $.getJSON("acs5_variables.json", function(data){
+        dataOptions = data;
+    }),
+    $.getJSON("schools.json",function(data) {
+        schools = data;
+    }),
 
-                //Init BaseMaps
-                var basemaps = {
-                  "Esri_WorldGrayCanvas": L.tileLayer(
-                    'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
-                    {
-
-	                  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-                      minZoom: 2,
-                      maxZoom: 19,
-                      opacity: 0.9,
-                      id: "osm.streets"
-                    }
-                  ),
-                  "Google-Map": L.tileLayer(
-                    "https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}",
-                    {
-                      minZoom: 2,
-                      maxZoom: 19,
-                      id: "google.street"
-                    }
-                  ),
-                  "Google-Satellite": L.tileLayer(
-                    "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-                    {
-                      minZoom: 2,
-                      maxZoom: 19,
-                      id: "google.satellite"
-                    }
-                  ),
-                  "Google-Hybrid": L.tileLayer(
-                    "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
-                    {
-                      minZoom: 2,
-                      maxZoom: 19,
-                      id: "google.hybrid"
-                    }
-                  )
-                };
-
-                //Map Options
-                var mapOptions = {
-                  zoomControl: false,
-                  attributionControl: false,
-                  center: [29.41148852531869, 261.5055084228516],
-                  zoom: 10,
-                  layers: [basemaps.Esri_WorldGrayCanvas]
-                };
+    $.getJSON("geo_2000.geojson", function(data) {
+        mdata = data;
+    })
+). then(uei_base);
 
 
-                var geojson = L.geoJson(data, {});
+// main page setup function
+function uei_base(){
+    // add moran's eye property
+    addGeojsonProp(mdata,"moran");
 
-                    //Render Main Map
-                    var map = L.map('my-map', mapOptions)
-                    .fitBounds(geojson.getBounds());
+    //Init Overlays
+    var overlays = {};
 
-                    geojson.addTo(map);
+    //Init BaseMaps
+    var basemaps = {
+      "Esri_WorldGrayCanvas": L.tileLayer(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+        {
 
-                //Render Zoom Control
-                L.control
-                  .zoom({
-                    position: "topleft"
-                  })
-                  .addTo(map);
+          attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+          minZoom: 2,
+          maxZoom: 19,
+          opacity: 0.9,
+          id: "osm.streets"
+        }
+      ),
+      "Google-Map": L.tileLayer(
+        "https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}",
+        {
+          minZoom: 2,
+          maxZoom: 19,
+          id: "google.street"
+        }
+      ),
+      "Google-Satellite": L.tileLayer(
+        "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+        {
+          minZoom: 2,
+          maxZoom: 19,
+          id: "google.satellite"
+        }
+      ),
+      "Google-Hybrid": L.tileLayer(
+        "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+        {
+          minZoom: 2,
+          maxZoom: 19,
+          id: "google.hybrid"
+        }
+      )
+    };
 
-                var sidebar = L.control
-                  .sidebar({
-                    autopan: false,
-                    container: "sidebar",
-                    position: "right"
-                  })
-                  .addTo(map);
-
-                //Render Layer Control & Move to Sidebar
-                var layerControl = L.control
-                  .layers(basemaps, overlays, {
-                    position: "topright",
-                    collapsed: false
-                  })
-                  .addTo(map);
-
-
-                // not sure about this code...
-                var oldLayerControl = layerControl.getContainer();
-                var newLayerControl = $("#layercontrol");
-                newLayerControl.append(oldLayerControl);
-                $(".leaflet-control-layers-list").prepend("<strong class='title'> Maps</strong><br>");
-                $(".leaflet-control-layers-separator").after("<br><strong class='title'>Layers</strong>");
-                $(".leaflet-control-layers-separator").after("<br><strong class='title'>Moran</strong>");
-                $('strong:contains("Moran")').after('<div class="leaflet-control-layers-separator" style=""></div>');
-
-
-                // add layer control for tracts and school
-                var overlay = new L.FeatureGroup();
-                layerControl.addOverlay(geojson, "census tracts");
-
-
-                // add menu options to select moran variables
-                var loader = "";
-                dataOptions.forEach(function (item,index) {
-                    loader += moranCheckboxHTML(item,index);
-                });
-
-                var button = '<div id="run_moran"><button type="button">Run Moran!</button></div>'
-                $('strong:contains("Moran")').after('<div class="leaflet-control-layers-overlays">' +
-                        loader +
-                        '</div>' + button);
-
-                // set check values
-                moranCheckboxSetup();
-
-                // get selected varaibles for moran's eye
-                var moran_variables = {
-                    "name": [],
-                    "val":[]
-                };
-
-                $().ready(function(){
-                    $("input.leaflet-control-moran-selector:checked").map(function() {
-                        moran_variables.name.push($(this).attr("name"));
-                        moran_variables.val.push($(this).val());
-                    });
-                    console.log(moran_variables);
-                    moranRun(data,moran_variables.val);
-                    geojson.eachLayer(style);
-                });
+    //Map Options
+    var mapOptions = {
+      zoomControl: false,
+      attributionControl: false,
+      center: [29.41148852531869, 261.5055084228516],
+      zoom: 10,
+      layers: [basemaps.Esri_WorldGrayCanvas]
+    };
 
 
-                $('#run_moran').click(function(){
-                    $("input.leaflet-control-moran-selector:checked").map(function() {
-                        moran_variables.name.push($(this).attr("name"));
-                        moran_variables.val.push($(this).val());
-                    });
-                    moranRun(data,moran_variables.val);
-                    console.log(moran_variables);
-                    geojson.eachLayer(style);
-                });
+    var geojson = L.geoJson(mdata, {});
 
-                function style(layer) {
-                    layer.setStyle({
-                        fillColor: getColor(layer.feature.properties.moran),
-                        weight: 2,
-                        opacity: 1,
-                        color: '#0E213D',
-                        dashArray: '3',
-                        fillOpacity: 0.7
-                    }),
-                    layer.bindPopup('<div><strong class="title">Tract: ' + layer.feature.properties.NAME +
-                        '</strong><strong class="title-right" > Total Score: ' +
-                        layer.feature.properties.moran +
-                        "</strong></div>" +
-                        buildMoranPopup(layer,moran_variables));
-                }
+        //Render Main Map
+        var map = L.map('my-map', mapOptions)
+        .fitBounds(geojson.getBounds());
+
+        geojson.addTo(map);
+
+    //Render Zoom Control
+    L.control
+      .zoom({
+        position: "topleft"
+      })
+      .addTo(map);
+
+    var sidebar = L.control
+      .sidebar({
+        autopan: false,
+        container: "sidebar",
+        position: "right"
+      })
+      .addTo(map);
+
+    //Render Layer Control & Move to Sidebar
+    var layerControl = L.control
+      .layers(basemaps, overlays, {
+        position: "topright",
+        collapsed: false
+      })
+      .addTo(map);
+
+
+    // not sure about this code...
+    var oldLayerControl = layerControl.getContainer();
+    var newLayerControl = $("#layercontrol");
+    newLayerControl.append(oldLayerControl);
+    $(".leaflet-control-layers-list").prepend("<strong class='title'> Maps</strong><br>");
+    $(".leaflet-control-layers-separator").after("<br><strong class='title'>Layers</strong>");
+    $(".leaflet-control-layers-separator").after("<br><strong class='title'>Moran</strong>");
+    $('strong:contains("Moran")').after('<div class="leaflet-control-layers-separator" style=""></div>');
+
+
+    // add layer control for tracts and school
+    var overlay = new L.FeatureGroup();
+    layerControl.addOverlay(geojson, "census tracts");
+
+
+    // add menu options to select moran variables
+    var loader = "";
+    dataOptions.forEach(function (item,index) {
+            loader += moranCheckboxHTML(item,index);
+    });
+
+    var button = '<div id="run_moran"><button type="button">Run Moran!</button></div>'
+    $('strong:contains("Moran")').after('<div class="leaflet-control-layers-overlays">' +
+            loader +
+            '</div>' + button);
+
+    // set check values
+    moranCheckboxSetup();
+
+    // get selected varaibles for moran's eye
+    var moran_variables = {
+        "name": [],
+        "val":[]
+    };
+
+    $().ready(function(){
+        $("input.leaflet-control-moran-selector:checked").map(function() {
+            moran_variables.name.push($(this).attr("name"));
+            moran_variables.val.push($(this).val());
+        });
+        console.log(moran_variables);
+        moranRun(mdata,moran_variables.val);
+        geojson.eachLayer(style);
+    });
+
+
+    $('#run_moran').click(function(){
+        $("input.leaflet-control-moran-selector:checked").map(function() {
+            moran_variables.name.push($(this).attr("name"));
+            moran_variables.val.push($(this).val());
+        });
+        moranRun(mdata,moran_variables.val);
+        console.log(moran_variables);
+        geojson.eachLayer(style);
+    });
+
+    function style(layer) {
+        layer.setStyle({
+            fillColor: getColor(layer.feature.properties.moran),
+            weight: 2,
+            opacity: 1,
+            color: '#0E213D',
+            dashArray: '3',
+            fillOpacity: 0.7
+        }),
+        layer.bindPopup('<div><strong class="title">Tract: ' + layer.feature.properties.NAME +
+            '</strong><strong class="title-right" > Total Score: ' +
+            layer.feature.properties.moran +
+            "</strong></div>" +
+            buildMoranPopup(layer,moran_variables));
+    }
+
+// add year slider and pull new year and weight matrix
 /*
-                    var geojson = L.geoJson(data, {
-                      onEachFeature: function (feature, layer) {
-                        layer.bindPopup("Tract: " + feature.properties.NAME + "<p><b> test: " + feature.properties.P003005 + "</b></p>");
-                        pub = feature;
-                      }
-                    });
+        var geojson = L.geoJson(mdata, {
+          onEachFeature: function (feature, layer) {
+            layer.bindPopup("Tract: " + feature.properties.NAME + "<p><b> test: " + feature.properties.P003005 + "</b></p>");
+            pub = feature;
+          }
+        });
 */
 
 
-                // set the layer colors
-                /*
-                geojson.eachLayer(function (layer) {
-                    if(layer.feature.properties.NAME == '1821.03') {
-                        layer.setStyle({fillColor :'blue'})
-                    }
-                });
-                */
-
-                // run moran's eye based on selected variaibles
-                // assign value to moran property in features >> properties
-
-                // set the layer colors
-                /*
-                geojson.eachLayer(function (feature) {
-                    feature.setStyle({
-                                    fillColor: getColor(feature.feature.properties.moran),
-                                    weight: 2,
-                                    opacity: 1,
-                                    color: 'white',
-                                    dashArray: '3',
-                                    fillOpacity: 0.7
-                                });
-                });
-                     //Set tract colors
-                    geojson.setStyle({color:"#32a866"})
-               */
-
-                // global variable assign for error checking
-                data_temp = data;
-                schools_temp = schools;
-                dataOptions_temp = dataOptions;
-                geojson_temp = geojson;
-
-        });
+    // set the layer colors
+    /*
+    geojson.eachLayer(function (layer) {
+        if(layer.feature.properties.NAME == '1821.03') {
+            layer.setStyle({fillColor :'blue'})
+        }
     });
-});
+    */
+
+    // run moran's eye based on selected variaibles
+    // assign value to moran property in features >> properties
+
+    // set the layer colors
+    /*
+    geojson.eachLayer(function (feature) {
+        feature.setStyle({
+                        fillColor: getColor(feature.feature.properties.moran),
+                        weight: 2,
+                        opacity: 1,
+                        color: 'white',
+                        dashArray: '3',
+                        fillOpacity: 0.7
+                    });
+    });
+         //Set tract colors
+        geojson.setStyle({color:"#32a866"})
+   */
+
+}
+
+
--- moran_local.js
+++ moran_local.js
@@ -1,6 +1,13 @@
-function moranTemp(dataset,geospatial,adjlist){
-    console.log("this is awesome");
+var dataset_temp, geospatial_temp;
+function moranTemp(dataset,geospatial){
     var moran_vector = [];
+    var dataset_means = [];
+
+    // temp for checking 
+    dataset_temp = dataset;
+    geospatial_temp = geospatial;
+   
+
     dataset.forEach(function(feature){
         moran_vector.push(parseFloat(Math.random().toFixed(2)));
     });
@@ -8,145 +15,47 @@ function moranTemp(dataset,geospatial,adjlist){
     return moran_vector;
 }
 
-const MI={
-    CalcLMI:function(dataset,geojson,adjlist, isbounds){
-        let MeanV = CalcMean(dataset)
-        let MIset = []
-        for(i=0;i<dataset.length;i++){
-            geojson[i].properties.LMI = CalcByi(i,MeanV,dataset,isbounds,geojson,adjlist)
-            MIset.push(CalcByi(i,MeanV,dataset,isbounds,geojson,adjlist))
-        }
-        return MIset
-    },
-    //calc Local Moran's I by classification result result
-    CalcLMI_byclustering:function(clusterdata){
-        let dataset = []
-        for(let i=0;i<clusterdata.length;i++){
-            for(let m=0;m<clusterdata[i].length;m++){
-                dataset[clusterdata[i][m]] = CreateVector(clusterdata.length, i)
-            }
-        }
-        return MI.CalcLMI(dataset)
-    },
-    rendering:function (){
-    }
-}
-
-
-function CalcByi(id,Mean,dataset,isbounds,geojson,adjlist){
-    let result=0;
-    let list
-    if(isbounds)
-        list = get_bound_weight(id,adjlist)
-
-    let thisvectors = []
-    list.forEach( (weight,thisid)=>{
-        thisvectors.push(dataset[thisid])
-    })
-
-    list.forEach( (weight,thisid)=>{
-        result = plusV(result, VmultiN(weight,minusV(dataset[id],Mean)))
-    })
-    result = multiV( result,minusV(dataset[id],Mean))
-
-    let sumequ = 0
-    geojson.forEach( (ob, i)=>{
-        if ( i != id){
-            let minusVector = minusV(dataset[i],Mean)
-            sumequ += multiV(minusVector, minusVector)
-        }
-    })
-    sumequ = sumequ / (geojson.length-1)
-    result = result / sumequ
-
-    //weight norm
-    let weightsum=0
-    list.forEach( (weight,thisid)=>{
-        weightsum += weight
-    })
-    if(weightsum==0)
-        return 0
-    else
-        return result / weightsum
-}
-
-// get weight by boundary
-function get_bound_weight(id,adjlist){
-    let result =new Map()
-    let thisvector = adjlist.get(id)
-
-    thisvector.forEach((d,i)=>{
-        if(i!=id)
-        result.set(i, thisvector.get(i)/thisvector.get(id))
-    })
-    return result
-}
-function CreateVector(length,label){
-    let vectors = []
-    for(let i=0;i<length;i++){
-        if(i==label)
-            vectors.push(1)
-        else
-            vectors.push(0)
-    }
-    return vectors;
-}
-function CalcMean(vectors){
-    let result=null;
-    for (let i=0;i<vectors.length;i++){
-        if(result==null)
-            result = $.extend(true, [], vectors[i])
-        else{
-            for(let m=0;m<vectors[i].length;m++){
-                result[m]+=vectors[i][m]
-            }
+function calcMean(arr_num){
+    var i,j;
+    arr_mean = [];
+    while(arr_num[0].length > arr_mean.length) arr_mean.push(0);
+    console.log(arr_mean)
+    for (i = 0; i < arr_num.length;i++) {
+        console.log("i is: "+i);
+        for(j=0;j < arr_mean.length;j++){
+            arr_mean[j] += arr_num[i][j];
+            console.log("j is: "+j);
         }
     }
-    for(let m=0;m<result.length;m++){
-        result[m] = result[m]/vectors.length
-    }
-    return result
-}
-function VmultiN(n,v){
-    let result = $.extend(true, [], v)
-    for(let i=0 ; i < result.length;i++){
-        result[i]*=n
-    }
-    return result
+    return arr_mean;
 }
 
-function multiV(v1,v2){
-    let result = 0;
-    for(let i=0;i<v1.length;i++){
-        result += v1[i]* v2[i]
-    }
-    return result
-}
-function minusV(v1,v2){
-    let result = $.extend(true, [], v1)
+function calcSd(arr_num){
 
-    for(let i=0 ; i < v1.length;i++){
-        result[i] = v1[i]*1-v2[i]*1
-    }
-    return result
-}
-function plusV(v1,v2){
-    if(v1==0)
-        return v2;
-    let result = new Array()
-    for(let i=0 ; i < v1.length;i++){
-        result[i] = v1[i]*1+v2[i]*1
-    }
-    return result;
 }
 
-$.getJSON("geo_2000.json")
-$.getJSON("weight_matrix.json"
-$.getJson("tract_indeces.json")
-$.getJson("morans_i_sd.json")
+function moranI(dataset,geospatial){
+var moran_vector = [];
+    var dataset_means = [];
 
+    for(var i = 0;dataset.length;i++){
+
+    }
+    // temp for checking 
+    dataset_temp = dataset;
+    geospatial_temp = geospatial;
+   
+    //filter weight_matrix
+
+
+    dataset.forEach(function(feature){
+        moran_vector.push(parseFloat(Math.random().toFixed(2)));
+    });
+
+    return moran_vector;
 // after variables are selected by user and a census tract (tract_i) is chosen,
 // assign the number of selected variables to 'n'.
+
 // import geo_year.json
 // filter file 'geo_xxxx.json' for selected variables
 // import weight_matrix.json
@@ -163,9 +72,5 @@ $.getJson("morans_i_sd.json")
 
 // for each variable, calculate I
 // get the year's matrix indeces, which show which index matches which census tract
-
+console.log("hey!");
 }
-
-//
-console.log(phrase)
-}}
